cbf7447050e2839078c7fde8741a4bd70d3d56a1
yongbeomkwak <yongbeomkwak@gmail.com>
2021-12-24 23:41:40 +0900

    Scaffold

--- Compose/app/src/main/java/com/yongbeom/studycompose/MainActivity.kt
+++ Compose/app/src/main/java/com/yongbeom/studycompose/MainActivity.kt
@@ -17,15 +17,19 @@ import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.yongbeom.studycompose.ui.theme.StudyComposeTheme
+import kotlinx.coroutines.launch
 
+@ExperimentalComposeUiApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,21 +39,49 @@ class MainActivity : ComponentActivity() {
                 // 초기값 false
                 mutableStateOf(false)
             }
-            StudyComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background)
-                {
-                    ImageCard(
-                        modifier = Modifier
-                            .fillMaxSize(0.5f)
-                            .padding(16.dp),
-                        isFavorite = isFavorite
-                    ) { favorite ->
-                        isFavorite = favorite //갑 변경
+            val (text,setValue) = remember {
+                //setValue는 함
+                mutableStateOf("")
+            }
+            val scaffoldState = rememberScaffoldState() //스낵 bar를 위해 사용
+            val scope= rememberCoroutineScope() // 스낵바는 코루틴 필요
+            val keyboardController=LocalSoftwareKeyboardController.current //키보드 컨트롤러
+            Scaffold(
+                scaffoldState = scaffoldState //스낵 바를 위한
+            ) {
+                StudyComposeTheme {
+                    // A surface container using the 'background' color from the theme
+                    Surface(color = MaterialTheme.colors.background)
+                    {
+                        ImageCard(
+                            modifier = Modifier
+                                .fillMaxSize(0.5f)
+                                .padding(16.dp),
+                            isFavorite = isFavorite
+                        ) { favorite ->
+                            isFavorite = favorite //갑 변경
 
+                        }
+                        Column(modifier = Modifier.fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally) {
+                            TextField(value = text, onValueChange = setValue)
+                            Button(onClick = {
+                                keyboardController?.hide() //키보드 숨기기
+                                scope.launch {
+                                    scaffoldState.snackbarHostState.showSnackbar("Hello $text")
+                                }
+                            }) {
+                                Text(text = "클릭")
+                            }
+
+                        }
                     }
                 }
             }
+
+
+
         }
     }
 }
@@ -61,8 +93,9 @@ fun ImageCard(
     onTabFavorite: (Boolean) -> Unit // 콜백함수 (매개변수 Boolean)
 ) {
     Card(
+        // 카드 뷰
         modifier = modifier,
-        shape = RoundedCornerShape(8.dp),
+        shape = RoundedCornerShape(8.dp), //둥근 모서리 모양
         elevation = 5.dp,
     ) {
         Box(
