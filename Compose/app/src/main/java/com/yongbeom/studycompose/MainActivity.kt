37e5d0c3ac01ff71da62d1850600a02db4f807c4
yongbeomkwak <yongbeomkwak@gmail.com>
2021-12-26 00:13:45 +0900

    Dev chapter 1

--- Compose/app/src/main/java/com/yongbeom/studycompose/MainActivity.kt
+++ Compose/app/src/main/java/com/yongbeom/studycompose/MainActivity.kt
@@ -1,85 +1,75 @@
 package com.yongbeom.studycompose
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yongbeom.studycompose.ui.theme.StudyComposeTheme
 
-
-class MainActivity:ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-           setContent {
-               HomeScreen(MainViewModelT())
-           }
+        setContent {
+            MyApp()
+        }
     }
 }
 
 @Composable
-fun HomeScreen(viewModel: MainViewModelT)
-{
-    // 총 4가지 방법으로 State 사용가능
-    val (text:String,setText:(String)->Unit) = remember{  //by를 사용하지 않으면 함수를 이용한다
-        //TextField에서 자주 사용
-        mutableStateOf("Hello World")
-    }
-
-    val text1:MutableState<String> = remember { // .value로 접근
-        mutableStateOf("Hello World")
-    }
-
-    var text2:String  by  remember {  // by를 이용하면 getter와 setter를 그냥 변수로 처리함 (델리게이트 이용)
-        mutableStateOf("Hello World")
-    }
-
-    val text3:State<String> =viewModel.liveData.observeAsState("Hello World")
-    
-    Column() {
-        Text(text = "Hello World")
-        Button(onClick = {
-            text1.value="변경1"
-            Log.e("OUTPUT",text1.value)
-            text2="변경2"
-            Log.e("OUTPUT",text2)
-            setText("변경")
-            Log.e("OUTPUT",text)
-            viewModel.changeValue("변경4")
-            Log.e("OUTPUT",viewModel.data.value)
-            viewModel.changeLiveData("HHHHH")
-            Log.e("OUTPUT",text3.value)
-
-        }) {
-            Text(text = "클릭")
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Surface(color = MaterialTheme.colors.background) {
+        Column {
+            for (name in names) {
+                Greeting(name = name)
+            }
         }
-        TextField(value = text, onValueChange =setText)
     }
 }
 
-class MainViewModelT:ViewModel()
-{
-    private val _data:MutableState<String> = mutableStateOf("Hello World")
-    val data:State<String> =_data
-
-    private val _liveData=MutableLiveData<String>()
-    val liveData:LiveData<String> =_liveData
-
-    fun changeValue(value:String)
-    {
-        _data.value=value
+@Composable
+private fun Greeting(name: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val extraPadding = if (expanded) 48.dp else 0.dp
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            )
+            {
+                Text(text = "Hello, ")
+                Text(text = name)
+            }
+            OutlinedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                Text(if (expanded) "Show less" else "Show more")
+            }
+        }
     }
-
-    fun changeLiveData(value: String)
-    {
-        _liveData.value=value
-    }
-
 }
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun DefaultPreview() {
+    StudyComposeTheme {
+        MyApp()
+    }
+}
+
+
---
+++ Compose/app/src/main/java/com/yongbeom/studycompose/StateTest.kt
@@ -0,0 +1,85 @@
+package com.yongbeom.studycompose
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class StateTest:ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+           setContent {
+               HomeScreen(MainViewModelT())
+           }
+    }
+}
+
+@Composable
+fun HomeScreen(viewModel: MainViewModelT)
+{
+    // 총 4가지 방법으로 State 사용가능
+    val (text:String,setText:(String)->Unit) = remember{  //by를 사용하지 않으면 함수를 이용한다
+        //TextField에서 자주 사용
+        mutableStateOf("Hello World")
+    }
+
+    val text1:MutableState<String> = remember { // .value로 접근
+        mutableStateOf("Hello World")
+    }
+
+    var text2:String  by  remember {  // by를 이용하면 getter와 setter를 그냥 변수로 처리함 (델리게이트 이용)
+        mutableStateOf("Hello World")
+    }
+
+    val text3:State<String> =viewModel.liveData.observeAsState("Hello World")
+    
+    Column() {
+        Text(text = "Hello World")
+        Button(onClick = {
+            text1.value="변경1"
+            Log.e("OUTPUT",text1.value)
+            text2="변경2"
+            Log.e("OUTPUT",text2)
+            setText("변경")
+            Log.e("OUTPUT",text)
+            viewModel.changeValue("변경4")
+            Log.e("OUTPUT",viewModel.data.value)
+            viewModel.changeLiveData("HHHHH")
+            Log.e("OUTPUT",text3.value)
+
+        }) {
+            Text(text = "클릭")
+        }
+        TextField(value = text, onValueChange =setText)
+    }
+}
+
+class MainViewModelT:ViewModel()
+{
+    private val _data:MutableState<String> = mutableStateOf("Hello World")
+    val data:State<String> =_data
+
+    private val _liveData=MutableLiveData<String>()
+    val liveData:LiveData<String> =_liveData
+
+    fun changeValue(value:String)
+    {
+        _data.value=value
+    }
+
+    fun changeLiveData(value: String)
+    {
+        _liveData.value=value
+    }
+
+}
