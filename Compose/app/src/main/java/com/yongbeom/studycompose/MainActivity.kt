5864c576c36f487979e5446e1f398330ecd5c320
yongbeomkwak <yongbeomkwak@gmail.com>
2021-12-25 21:55:03 +0900

    State 심화

--- Compose/app/src/main/java/com/yongbeom/studycompose/MainActivity.kt
+++ Compose/app/src/main/java/com/yongbeom/studycompose/MainActivity.kt
@@ -1,51 +1,85 @@
 package com.yongbeom.studycompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class MainActivity : ComponentActivity() {
 
+class MainActivity:ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-             val viewModel by viewModels<MainViewModel>() //뷰 모델 정의
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                Text(text=viewModel.data.value, fontSize = 30.sp)
-                Button(onClick = {
-                    viewModel.changeValue()
-                }) {
-                    Text(text = "변경")
-                }
-
-            }
-        }
+           setContent {
+               HomeScreen(MainViewModelT())
+           }
     }
 }
 
-class MainViewModel : ViewModel() {
-    private val _data = mutableStateOf("Hello") // 데이터는 private
-    val data:State<String> =_data //외부로는 이렇게 제공
-
-    fun changeValue() //변경 메소드
-    {
-        _data.value="World"
+@Composable
+fun HomeScreen(viewModel: MainViewModelT)
+{
+    // 총 4가지 방법으로 State 사용가능
+    val (text:String,setText:(String)->Unit) = remember{  //by를 사용하지 않으면 함수를 이용한다
+        //TextField에서 자주 사용
+        mutableStateOf("Hello World")
     }
+
+    val text1:MutableState<String> = remember { // .value로 접근
+        mutableStateOf("Hello World")
+    }
+
+    var text2:String  by  remember {  // by를 이용하면 getter와 setter를 그냥 변수로 처리함 (델리게이트 이용)
+        mutableStateOf("Hello World")
+    }
+
+    val text3:State<String> =viewModel.liveData.observeAsState("Hello World")
+    
+    Column() {
+        Text(text = "Hello World")
+        Button(onClick = {
+            text1.value="변경1"
+            Log.e("OUTPUT",text1.value)
+            text2="변경2"
+            Log.e("OUTPUT",text2)
+            setText("변경")
+            Log.e("OUTPUT",text)
+            viewModel.changeValue("변경4")
+            Log.e("OUTPUT",viewModel.data.value)
+            viewModel.changeLiveData("HHHHH")
+            Log.e("OUTPUT",text3.value)
+
+        }) {
+            Text(text = "클릭")
+        }
+        TextField(value = text, onValueChange =setText)
+    }
+}
+
+class MainViewModelT:ViewModel()
+{
+    private val _data:MutableState<String> = mutableStateOf("Hello World")
+    val data:State<String> =_data
+
+    private val _liveData=MutableLiveData<String>()
+    val liveData:LiveData<String> =_liveData
+
+    fun changeValue(value:String)
+    {
+        _data.value=value
+    }
+
+    fun changeLiveData(value: String)
+    {
+        _liveData.value=value
+    }
+
 }
---
+++ Compose/app/src/main/java/com/yongbeom/studycompose/ViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.yongbeom.studycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.ViewModel
+
+class ViewModelTest : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+             val viewModel by viewModels<MainViewModel>() //뷰 모델 정의
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(text=viewModel.data.value, fontSize = 30.sp)
+                Button(onClick = {
+                    viewModel.changeValue()
+                }) {
+                    Text(text = "변경")
+                }
+
+            }
+        }
+    }
+}
+
+class MainViewModel : ViewModel() {
+    private val _data = mutableStateOf("Hello") // 데이터는 private mutableStateOf 쓰기 읽기 가능
+    val data:State<String> =_data //외부로는 이렇게 제공 (읽기만 가능)
+
+    fun changeValue() //변경 메소드
+    {
+        _data.value="World"
+    }
+}
