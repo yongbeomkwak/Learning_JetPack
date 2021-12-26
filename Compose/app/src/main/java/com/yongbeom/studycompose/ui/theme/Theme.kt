bc463368575ac7c68280288be9acdbdc9ba4e836
yongbeomkwak <yongbeomkwak@gmail.com>
2021-12-26 19:45:49 +0900

    Icon with Anim

--- Compose/app/src/main/java/com/yongbeom/studycompose/ui/theme/Theme.kt
+++ Compose/app/src/main/java/com/yongbeom/studycompose/ui/theme/Theme.kt
@@ -1,44 +1,44 @@
 package com.yongbeom.studycompose.ui.theme
 
+import android.graphics.Color
+import android.graphics.Color.WHITE
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
 
+//다크모드
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
 )
 
+//주간모
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    surface = Blue,
+    onSurface = White,
+    primary = LightBlue,
+    onPrimary = Navy
 )
+
 
 @Composable
 fun StudyComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable() () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
+    val colors = if (darkTheme) { //만약 다크모드면
+        DarkColorPalette //다크
     } else {
         LightColorPalette
     }
 
-    MaterialTheme(
+
+    MaterialTheme( //실질적용 적용 코드
         colors = colors,
         typography = Typography,
         shapes = Shapes,
